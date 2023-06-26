587cf4894fb8e7746790a49b826cf4d11a1ef9c1
Giovanni Lopes <giovanniclopes@gmail.com>
2023-06-26 11:35:21 -0300

    scroll-to-top button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div>
-        <BrowserRouter>
+      <BrowserRouter>
         <Header />
-          <Router />
-        </BrowserRouter>
+        <ScrollToTop />
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
